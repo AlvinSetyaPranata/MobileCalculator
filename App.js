8edc6478817c6b0e786128f33343331e22f162c9
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-05-28 16:39:15 +0700

    added basic controlled input handler

--- App.js
+++ App.js
@@ -1,90 +1,153 @@
-import { useReducer } from 'react';
-import { Pressable, StyleSheet, Text, View } from 'react-native';
-import { TextInput } from 'react-native-web';
+import { useReducer } from "react";
+import { StyleSheet, Text, View, Pressable } from "react-native";
+import { TextInput } from "react-native-web";
 
+
+function Button({children, onPress=() => {}}, ) { 
+
+  return (
+    <Pressable style={styles.button} onPress={onPress}>
+      <Text>{children}</Text>
+    </Pressable>
+  )
+}
+ 
 export default function App() {
 
-
-  const [displayState, displayDispatch] = useReducer(displayReducer, {value: 0})
-
   const displayReducer = (state, action) => {
-    switch(action.type) {
-      case 'ADD':
+    switch (action.type) {
+      case "ADD":
         return {
-          value: state + action.newValue
-        }
-        break
-      case 'SUB':
+          value: state.value + action.newValue,
+        };
+      case "SUB":
         return {
-          value: state - action.newValue
-        }
-        break
-      case 'MUL':
+          value: state.value - action.newValue,
+        };
+      case "MUL":
         return {
-          value: state - action.newValue
-        }
-        break
-      case 'DIV':
+          value: state.value - action.newValue,
+        };
+      case "DIV":
         return {
-          value: state - action.newValue
-        }
-        break
-      case 'CAL':
+          value: state.value - action.newValue,
+        };
+      case "CAL":
         return {
-          value: state - action.newValue
-        }
-        break
-      case 'CLR':
+          value: state.value - action.newValue,
+        };
+      case "CLR":
         return {
-          value: state - action.newValue
-        }
-        break
-      case 'DEL':
+          value: 0,
+        };
+      case "DEL":
         return {
-          value: state - action.newValue
+          value: state.value - action.newValue,
+        };
+
+      case 'CHANGE':
+        return {
+          value: action.display
+        };
+
+      case 'DEC':
+        return {
+          value: 0
         }
-        break
+
+      case "PUT":
+        if (state == "0" || state == "") {
+          return {
+            display: action.newValue,
+          };
+        } else {
+          return {
+            display: state.value + action.newValue,
+          };
+        }
     }
-  }
+  };
+
+  const [displayState, displayDispatch] = useReducer(displayReducer, {
+    display: "",
+    values: []
+  });
 
   return (
     <View style={styles.container}>
-      <Text style={styles.heading}>Kalkulatorku</Text>  
-      <TextInput style={styles.input} type="number" value={display}/>
-      <TextInput style={styles.operatorInput} type="number"/>
+      <Text style={styles.heading}>Kalkulatorku</Text>
+      <TextInput
+        style={styles.input}
+        type="number"
+        value={displayState.value}
+        onChange={event => displayDispatch({type: 'CHANGE', display: event.value})}
+      />
+      <TextInput style={styles.operatorInput} type="number" />
 
       <View style={styles.buttonContainer}>
         <View style={styles.buttonRow}>
-          <Pressable style={styles.button}>CE</Pressable>
-          <Pressable style={styles.button}>+/-</Pressable>
-          <Pressable style={styles.button}>Erase</Pressable>
-          <Pressable style={styles.button}>÷</Pressable>
+          <Button style={styles.button}>CE</Button>
+          <Button style={styles.button}>+/-</Button>
+          <Button style={styles.button}>Erase</Button>
+          <Button style={styles.button}>÷</Button>
         </View>
         <View style={styles.buttonRow}>
-          <Pressable style={styles.button} onPress={() => update("7")}>7</Pressable>
-          <Pressable style={styles.button} onPress={() => update("8")}>8</Pressable>
-          <Pressable style={styles.button} onPress={() => update("9")}>9</Pressable>
-          <Pressable style={styles.button} onPress={() => update("X")}>X</Pressable>
+          <Button
+            style={styles.button}
+            onPress={() => displayDispatch({ type: "PUT", newValue: 7 })}
+          >
+            7
+          </Button>
+          <Button style={styles.button} onPress={() => displayDispatch({ type: "PUT", newValue: 8 })}>
+            8
+          </Button>
+          <Button style={styles.button} onPress={() => displayDispatch({ type: "PUT", newValue: 9 })}>
+            9
+          </Button>
+          <Button style={styles.button} onPress={() => displayDispatch({ type: "MUL", newValue: null})}>
+            X
+          </Button>
         </View>
         <View style={styles.buttonRow}>
-          <Pressable style={styles.button} onPress={() => update("4")}>4</Pressable>
-          <Pressable style={styles.button} onPress={() => update("5")}>5</Pressable>
-          <Pressable style={styles.button} onPress={() => update("6")}>6</Pressable>
-          <Pressable style={styles.button} onPress={() => update("-")}>-</Pressable>
+          <Button style={styles.button} onPress={() => displayDispatch({ type: "PUT", newValue: 4 })}>
+            4
+          </Button>
+          <Button style={styles.button} onPress={() => displayDispatch({ type: "PUT", newValue: 5 })}>
+            5
+          </Button>
+          <Button style={styles.button} onPress={() => console.log("6")}>
+            6
+          </Button>
+          <Button style={styles.button} onPress={() => displayDispatch({ type: "SUB", newValue: null })}>
+            -
+          </Button>
         </View>
         <View style={styles.buttonRow}>
-          <Pressable style={styles.button} onPress={() => update("1")}>1</Pressable>
-          <Pressable style={styles.button} onPress={() => update("2")}>2</Pressable>
-          <Pressable style={styles.button} onPress={() => update("3")}>3</Pressable>
-          <Pressable style={styles.button} onPress={() => update("+")}>+</Pressable>
+          <Button style={styles.button} onPress={() => displayDispatch({ type: "PUT", newValue: 1 })}>
+            1
+          </Button>
+          <Button style={styles.button} onPress={() => displayDispatch({ type: "PUT", newValue: 2 })}>
+            2
+          </Button>
+          <Button style={styles.button} onPress={() => displayDispatch({ type: "PUT", newValue: 3 })}>
+            3
+          </Button>
+          <Button style={styles.button} onPress={() => displayDispatch({ type: "ADD", newValue: null })}>
+            +
+          </Button>
         </View>
         <View style={styles.buttonRow}>
-          <Pressable style={styles.buttonSpan} onPress={() => update("0")}>0</Pressable>
-          <Pressable style={styles.button} onPress={() => update(".")}>.</Pressable>
-          <Pressable style={styles.button} onPress={() => update("=")}>=</Pressable>
+          <Button style={styles.buttonSpan} onPress={() => displayDispatch({ type: "PUT", newValue: 0 })}>
+            0
+          </Button>
+          <Button style={styles.button} onPress={() => displayDispatch({ type: "PUT", newValue: '.' })}>
+            .
+          </Button>
+          <Button style={styles.button} onPress={() => displayDispatch({ type: "CAL", newValue: null })}>
+            =
+          </Button>
         </View>
       </View>
-
     </View>
   );
 }
@@ -92,60 +155,59 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     padding: 20,
-    flex: 1
+    flex: 1,
   },
 
   heading: {
     fontSize: 20,
-    fontWeight: 'bold'
+    fontWeight: "bold",
   },
 
   input: {
-    width: '100%',
+    width: "100%",
     marginTop: 30,
     fontSize: 36,
-    textAlign: 'right',
-    fontWeight: 'bold',
+    textAlign: "right",
+    fontWeight: "bold",
   },
 
   operatorInput: {
-    width: '100%',
-    color: 'grey',
+    width: "100%",
+    color: "grey",
     marginTop: 10,
     fontSize: 24,
-    textAlign: 'right'
+    textAlign: "right",
   },
 
   buttonContainer: {
     flex: 4,
-    maxWidth: '400px',
+    maxWidth: "400px",
     rowGap: 10,
-    marginTop: 40
-
+    marginTop: 40,
   },
 
   buttonRow: {
-    flexDirection: 'row',
-    columnGap: 6
+    flexDirection: "row",
+    columnGap: 6,
   },
 
   button: {
     width: 100,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     height: 100,
     fontSize: 16,
-    backgroundColor: 'grey',
-    borderRadius: 10
+    backgroundColor: "grey",
+    borderRadius: 10,
   },
-  
+
   buttonSpan: {
     width: 206,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     height: 100,
     fontSize: 16,
-    backgroundColor: 'grey',
-    borderRadius: 10
+    backgroundColor: "grey",
+    borderRadius: 10,
   },
 });
