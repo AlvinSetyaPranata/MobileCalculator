2df477252463f47c19d966b582a68ee466533608
AlvinSetyaPranata <comand.alvin@gmail.com>
2024-05-28 08:09:23 +0700

    Created some UI

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { TextInput } from 'react-native-web';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.heading}>Kalkulatorku</Text>  
+      <TextInput style={styles.input} type="number"/>
+      <TextInput style={styles.operatorInput} type="number"/>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
+    flex: 1
   },
+
+  heading: {
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
+
+  input: {
+    width: '100%',
+    backgroundColor: 'red',
+    marginTop: 10,
+    fontSize: 28,
+    textAlign: 'right'
+  },
+
+  operatorInput: {
+    width: '100%',
+    color: 'grey',
+    marginTop: 10,
+    fontSize: 24,
+    textAlign: 'right'
+  }
 });
